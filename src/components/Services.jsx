4815353d73b979f1dd6e3565770750a6d86bca76
juanequex <ramon.g.uz37@gmail.com>
2022-06-17 17:55:34 -0500

    Added services images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,24 @@
 import { FaArchive } from 'react-icons/fa'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
-import Test from '../files/images/img2.jpeg'
+import TreeRemoval from '../files/images/tree-removal.png'
+import Lanscaper from '../files/images/landscaper.jpeg'
+import ConcreteMansory from '../files/images/concrete-mansory.jpeg'
+import Decoration from '../files/images/decoration.jpeg'
+import GardenDesign from '../files/images/garden-design.jpeg'
+import LawnCare from '../files/images/lawn-care.jpeg'
+import LawnLandscaping from '../files/images/lawn-landscaping.jpeg'
+import StoneLandscaping from '../files/images/stone-landscaping.jpeg'
+import StoneMansory from '../files/images/stone-mansory.jpeg'
+import TreeLandscaping from '../files/images/tree-landscaping.jpeg'
+import Irritating from '../files/images/irritating.webp'
+import Snow from '../files/images/snow.jpeg'
+import Treeplanting from '../files/images/tree-planting.webp'
+import FlowerBeds from '../files/images/flowerbeds.jpeg'
+import Spring from '../files/images/spring-clean-up.jpeg'
+import FlagStone from '../files/images/flag-stone.jpeg'
+import FallClean from '../files/images/fall-clean.jpeg'
+import HangingBaskets from '../files/images/hanging-baskets.jpeg'
 
 const responsive = {
   superLargeDesktop: {
@@ -24,24 +41,24 @@ const responsive = {
 }
 
 const listServices = [
-  { title: 'Landscaper', bg: Test },
-  { title: 'Concrete Masonry', bg: Test },
-  { title: 'Garden Decorating', bg: Test },
-  { title: 'Garden Design', bg: Test },
-  { title: 'Lawn Care', bg: Test },
-  { title: 'Lawn Lanscaping', bg: Test },
-  { title: 'Stone Landscaping', bg: Test },
-  { title: 'Stone Masonry', bg: Test },
-  { title: 'Tree Landscaping', bg: Test },
-  { title: 'Irritating', bg: Test },
-  { title: 'Snow Removal', bg: Test },
-  { title: 'Tree Planting', bg: Test },
-  { title: 'Tree Removal', bg: Test },
-  { title: 'Flowerbeds', bg: Test },
-  { title: 'Flagstone', bg: Test },
-  { title: 'Spring Clean up', bg: Test },
-  { title: 'Fall Clean Up', bg: Test },
-  { title: 'Hanging Baskets', bg: Test }
+  { title: 'Landscaper', bg: Lanscaper },
+  { title: 'Concrete Masonry', bg: ConcreteMansory },
+  { title: 'Garden Decorating', bg: Decoration },
+  { title: 'Garden Design', bg: GardenDesign },
+  { title: 'Lawn Care', bg: LawnCare },
+  { title: 'Lawn Lanscaping', bg: LawnLandscaping },
+  { title: 'Stone Landscaping', bg: StoneLandscaping },
+  { title: 'Stone Masonry', bg: StoneMansory },
+  { title: 'Tree Landscaping', bg: TreeLandscaping },
+  { title: 'Irritating', bg: Irritating },
+  { title: 'Snow Removal', bg: Snow },
+  { title: 'Tree Planting', bg: Treeplanting },
+  { title: 'Tree Removal', bg: TreeRemoval },
+  { title: 'Flowerbeds', bg: FlowerBeds },
+  { title: 'Flagstone', bg: FlagStone },
+  { title: 'Spring Clean up', bg: Spring },
+  { title: 'Fall Clean Up', bg: FallClean },
+  { title: 'Hanging Baskets', bg: HangingBaskets }
 ]
 
 const CardWithIcon = ({ icon = <FaArchive className='text-white' />, title, bg }) => {
